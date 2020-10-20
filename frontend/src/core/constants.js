b04a6cfe2c9a4139ee86136a76dbe1046f15cecb
Tom Taylor <hello@tomtaylor.name>
2020-10-20 11:50:36 +0100

    Fix config route

--- frontend/src/core/constants.js
+++ frontend/src/core/constants.js
@@ -1,8 +1,11 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
-define(['require', 'core/origin'], function(require, Origin, configJson) {
+define(['require', 'core/origin', 'core/utils'], function(require, Origin, Utils) {
   // Read in the configuration values/constants
-  $.getJSON('config/config.json', function(configData) {
-    Origin.constants = configData;
-    Origin.trigger('constants:loaded');
+  Utils.fetch('/api/config', (error, data) => {
+    if(error) {
+      return console.error(error.message);
+    }
+    Origin.constants = data;
+    Origin.trigger('constants:loaded', data);
   });
 });
