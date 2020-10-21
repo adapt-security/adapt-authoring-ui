95e6e383677b30461a5078eef2374ffc2b5f1ed8
Tom Taylor <hello@tomtaylor.name>
2020-10-21 17:40:18 +0100

    Refactor to use new API

--- app/modules/editor/themeEditor/models/editorModel.js
+++ app/modules/editor/themeEditor/models/editorModel.js
@@ -14,109 +14,49 @@ define(function(require) {
       },
 
       loadedData: function() {
-        if (this._siblings) {
-          this._type = this._siblings;
-        }
+        if(this._siblings) this._type = this._siblings;
         Origin.trigger('editorModel:dataLoaded', this._type);
       },
 
-      getChildren: function() {
-        var children = Origin.editor.data[this._children] || null;
-
-        return children && new Backbone.Collection(children.where({
-          _parentId: this.get("_id")
-        }));
-      },
-
       getParent: function() {
-        var currentParentId = this.get('_parentId');
-        var data = Origin.editor.data;
-        var course = data.course;
-
-        switch (this.get('_type')) {
-          case 'course':
-            return;
-          case 'menu':
-          case 'page':
-            return currentParentId === course.get('_id') ?
-                course :
-                data.contentObjects.findWhere({ _id: currentParentId });
-          default:
-            return data[this._parent].findWhere({ _id: currentParentId });
+        if(this.get('_type') !== 'course') {
+          return Origin.editor.data.content.findWhere({ _id: this.get('_parentId') });
         }
       },
 
       getSiblings: function(shouldIncludeSelf) {
-        var siblings = Origin.editor.data[this._siblings].where({
-          _parentId: this.get('_parentId')
-        });
-
-        if (!shouldIncludeSelf) {
-          siblings = siblings.filter(function(model) {
-            return model.get('_id') !== this.get('id');
-          }, this);
-        }
-
+        var siblings = Origin.editor.data.content.where({ _parentId: this.get('_parentId') });
+        if (!shouldIncludeSelf) siblings.remove(this.get('id'));
         return new Backbone.Collection(siblings);
       },
 
-      setOnChildren: function(key, value, options) {           
-        var args = arguments;
-        
-        if (!this._children) return;
-        
-        this.getChildren().each(function(child) {
-            child.setOnChildren.apply(child, args);
-        });
+      getChildren: function() {
+        return new Backbone.Collection(Origin.editor.data.content.where({ _parentId: this.get("_id") }));
       },
 
-      getPossibleAncestors: function() {
-        var map = {
-          'contentObjects': {
-            'ancestorType': 'page' 
-          },
-          'articles': {
-            'ancestorType': 'article'
-          },
-          'blocks': {
-            'ancestorType': 'block'
-          }
-        };
-        ancestors = Origin.editor.data[this._parent].where({_type: map[this._parent].ancestorType});
-        var ancestorsCollection = new Backbone.Collection(ancestors);
-        return ancestorsCollection;
+      setOnChildren: function(key, value, options) {           
+        if(this._children) {
+          this.getChildren().each(function(child) { child.setOnChildren.apply(child, arguments); });
+        }
+      },
+      /**
+       * Remove any attributes which are not on the whitelist (useful to call before a save)
+       */
+      pruneAttributes: function() {
+        if(this.whitelistAttributes) { 
+          Object.keys(this.attributes).forEach(function(key) {
+            if (!_.contains(this.whitelistAttributes, key)) this.unset(key);
+          }, this);
+        }
       },
 
       serialize: function() {
         return JSON.stringify(this);
       },
 
-      // Remove any attributes which are not on the whitelist
-      // Useful to call before a save()
-      pruneAttributes: function() {
-        if (!this.whitelistAttributes) return;
-
-        // Ensure that only valid attributes are pushed back on the save
-        Object.keys(this.attributes).forEach(function(key) {
-          if (!_.contains(this.whitelistAttributes, key)) {
-            this.unset(key);
-          }
-        }, this);
-      },
-
       serializeChildren: function() {
-        var children = this.getChildren();
-        var serializedJson = '';
-
-        if (children) {
-          children.models.forEach(function(child) {
-            serializedJson += child.serialize();
-          });  
-        }
-
-        return serializedJson;
+        return this.getChildren().reduce(function(s,c) { return s + c.serialize(); }, '');  
       }
-      
     });
 
     return EditorModel;
