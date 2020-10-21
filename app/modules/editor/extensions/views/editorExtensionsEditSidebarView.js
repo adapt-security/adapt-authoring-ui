2fabe3bd600cd21deda5299846b0f462d72f3fee
Tom Taylor <hello@tomtaylor.name>
2020-10-21 19:05:42 +0100

    Remove unused imports

--- app/modules/editor/extensions/views/editorExtensionsEditSidebarView.js
+++ app/modules/editor/extensions/views/editorExtensionsEditSidebarView.js
@@ -1,7 +1,5 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Backbone = require('backbone');
-  var Origin = require('core/origin');
   var SidebarItemView = require('modules/sidebar/views/sidebarItemView');
 
   var EditorExtensionsEditSidebarView = SidebarItemView.extend({}, {
