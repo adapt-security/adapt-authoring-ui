6226ddc0c7f732ad45b5bd0bee169e312025319a
Tom Taylor <hello@tomtaylor.name>
2020-10-22 13:25:12 +0100

    Sort imports alphabetically

--- app/modules/user/models/userProfileModel.js
+++ app/modules/user/models/userProfileModel.js
@@ -1,9 +1,9 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
+  var _ = require('underscore');
   var Backbone = require('backbone');
   var Helpers = require('core/helpers');
   var Origin = require('core/origin');
-  var _ = require('underscore');
 
   var UserProfileModel = Backbone.Model.extend({
     idAttribute: '_id',
