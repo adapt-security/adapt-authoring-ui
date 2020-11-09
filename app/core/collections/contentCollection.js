7844be54c0b7b71a12c3a448b07a14ec0f1b3756
Tom Taylor <hello@tomtaylor.name>
2020-11-09 13:46:00 +0000

    Refactor collection to use new abstract class

--- app/core/collections/contentCollection.js
+++ app/core/collections/contentCollection.js
@@ -1,39 +1,32 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define([
-  'backbone',
-  'underscore',
+  '../collections/apiCollection',
   '../models/contentModel',
   '../origin',
   '../helpers'
-], function(Backbone, _, ContentModel, Origin, Helpers) {
-  var ContentCollection = Backbone.Collection.extend({
+], function(ApiCollection, ContentModel, Origin, Helpers) {
+  var ContentCollection = ApiCollection.extend({
     url: 'api/content',
     model: ContentModel,
 
     initialize : function(models, options) {
+      ApiCollection.prototype.initialize.apply(this, arguments);
+    
       this._type = options._type;
       if(this._type) this.model = Helpers.contentModelMap(this._type);
       this._courseId = options._courseId;
       this._parentId = options._parentId;
-      this.customQuery = options.filter || {};
     
       this.on('reset', function() {
         Origin.trigger('contentCollection:dataLoaded', this._type);
       }, this);
     },
     buildQuery: function() {
-      var query = _.assign({}, this.customQuery);
+      var query = ApiCollection.prototype.buildQuery.apply(this, arguments);
       if(this._type) query._type = this._type;
       if(this._courseId) query._courseId = this._courseId;
       if(this._parentId) query._parentId = this._parentId;
       return query;
-    },
-    fetch: function(options) {
-      Backbone.Collection.prototype.fetch.call(this, _.assign({
-        url: 'api/content/query',
-        method: 'POST',
-        data: this.buildQuery()
-      }, options));
     }
   });
 
