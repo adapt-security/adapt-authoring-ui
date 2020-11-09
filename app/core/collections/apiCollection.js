dc0e81c8999be4774fcdf44f2b9067eef5be7399
Tom Taylor <hello@tomtaylor.name>
2020-11-09 13:45:48 +0000

    Add abstract collection class for DRY reasons

---
+++ app/core/collections/apiCollection.js
@@ -0,0 +1,22 @@
+// LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
+define(['backbone', 'underscore'], function(Backbone, _) {
+  var ApiCollection = Backbone.Collection.extend({
+    initialize : function(models, options) {
+      if(!this.url) this.url = options.url;
+      this.customQuery = options.filter || {};
+    },
+    buildQuery: function() {
+      var query = _.assign({}, this.customQuery);
+      return query;
+    },
+    fetch: function(options) {
+      Backbone.Collection.prototype.fetch.call(this, _.assign({
+        url: this.url + '/query',
+        method: 'POST',
+        data: this.buildQuery()
+      }, options));
+    }
+  });
+
+  return ApiCollection;
+});
