657ed622965fa78ca538e970e5e628853dbea445
Tom Taylor <hello@tomtaylor.name>
2020-11-10 18:00:18 +0000

    Add missing import

--- app/modules/editor/global/views/editorOriginView.js
+++ app/modules/editor/global/views/editorOriginView.js
@@ -1,5 +1,6 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require){
+  var ContentCollection = require('core/collections/contentCollection');
   var Origin = require('core/origin');
   var OriginView = require('core/views/originView');
 
