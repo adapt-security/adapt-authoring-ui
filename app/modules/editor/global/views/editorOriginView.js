1a3973c890f0a5dc0f8d24bb407f60aaff475ebf
Tom Taylor <hello@tomtaylor.name>
2020-11-19 14:09:26 +0000

    Refactor into template string

--- app/modules/editor/global/views/editorOriginView.js
+++ app/modules/editor/global/views/editorOriginView.js
@@ -159,7 +159,7 @@ define(function(require){
     buildErrorMessage: function(errorObjs, message = "") {
       _.each(errorObjs, function(item, key) {
         if(item.hasOwnProperty('message')) {
-          message += '<span class="key">' + (item.title || key) + '</span>: ' + item.message + '<br/>';
+          message += `<span class="key">${item.title || key}</span>: ${item.message}<br/>`;
         } else if(_.isObject(item)) { // recurse
           message = this.buildErrorMessage(item, message);
         }
