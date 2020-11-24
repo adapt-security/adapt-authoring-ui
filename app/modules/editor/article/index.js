3bc58d0f35afdf9c4db057c4758766ae6e4cf0b6
Tom Taylor <hello@tomtaylor.name>
2020-11-24 16:56:13 +0000

    Refactor to remove unnecessary fetch

--- app/modules/editor/article/index.js
+++ app/modules/editor/article/index.js
@@ -1,23 +1,18 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Origin = require('core/origin');
-  var Helpers = require('../global/helpers');
-
-  var ArticleModel = require('core/models/articleModel');
   var EditorArticleEditSidebarView = require('./views/editorArticleEditSidebarView');
   var EditorArticleEditView = require('./views/editorArticleEditView');
+  var Helpers = require('../global/helpers');
+  var Origin = require('core/origin');
 
-  Origin.on('editor:article', function(data) {
+  Origin.on('editor:article', async function(data) {
     if(data.action !== 'edit') {
       return;
     }
-    (new ArticleModel({ _id: data.id })).fetch({
-      success: async function(model) {
-        var form = await Origin.scaffold.buildForm({ model: model });
-        Helpers.setPageTitle(model);
-        Origin.sidebar.addView(new EditorArticleEditSidebarView({ model: model, form: form }).$el);
-        Origin.contentPane.setView(EditorArticleEditView, { model: model, form: form });
-      }
-    });
+    var model = Origin.editor.data.content.findWhere({ _id: data.id });
+    var form = await Origin.scaffold.buildForm({ model });
+    Helpers.setPageTitle(model);
+    Origin.sidebar.addView(new EditorArticleEditSidebarView({ model, form }).$el);
+    Origin.contentPane.setView(EditorArticleEditView, { model, form });
   });
 });
