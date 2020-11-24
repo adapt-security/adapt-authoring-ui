319c42ea3e8cfc56b9dba57b0a11616f7bae1889
Tom Taylor <hello@tomtaylor.name>
2020-11-24 17:23:20 +0000

    Refactor file to remove unnecessary API fetch

--- app/modules/editor/block/index.js
+++ app/modules/editor/block/index.js
@@ -1,24 +1,18 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Backbone = require('backbone');
-  var Origin = require('core/origin');
-  var Helpers = require('../global/helpers');
-
-  var BlockModel = require('core/models/blockModel');
-  var EditorBlockEditView = require('./views/editorBlockEditView');
   var EditorBlockEditSidebarView = require('./views/editorBlockEditSidebarView');
+  var EditorBlockEditView = require('./views/editorBlockEditView');
+  var Helpers = require('../global/helpers');
+  var Origin = require('core/origin');
 
-  Origin.on('editor:block', function(data) {
+  Origin.on('editor:block', async function(data) {
     if(data.action !== 'edit') {
       return;
     }
-    (new BlockModel({ _id: data.id })).fetch({
-      success: async function(model) {
-        var form = await Origin.scaffold.buildForm({ model: model });
-        Helpers.setPageTitle(model);
-        Origin.sidebar.addView(new EditorBlockEditSidebarView({ model: model, form: form }).$el);
-        Origin.contentPane.setView(EditorBlockEditView, { model: model, form: form });
-      }
-    });
+    var model = Origin.editor.data.findWhere({ _id: data.id });
+    var form = await Origin.scaffold.buildForm({ model });
+    Helpers.setPageTitle(model);
+    Origin.sidebar.addView(new EditorBlockEditSidebarView({ model, form }).$el);
+    Origin.contentPane.setView(EditorBlockEditView, { model, form });
   });
 });
