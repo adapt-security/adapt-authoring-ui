ea314e9589c58bfde8e6e785f29b612957034dcd
Tom Taylor <hello@tomtaylor.name>
2020-11-24 16:52:35 +0000

    Update and reorder importd

--- app/modules/editor/global/views/editorView.js
+++ app/modules/editor/global/views/editorView.js
@@ -3,18 +3,15 @@
  * TODO I think this exists to add extra functionality to the menu/page structure pages
  */
 define(function(require) {
-  var Backbone = require('backbone');
-  var Origin = require('core/origin');
-  var helpers = require('core/helpers');
-
-  var EditorOriginView = require('./editorOriginView');
-  var EditorMenuView = require('../../contentObject/views/editorMenuView');
-  var EditorPageView = require('../../contentObject/views/editorPageView');
-
-  var ContentObjectModel = require('core/models/contentObjectModel');
   var ArticleModel = require('core/models/articleModel');
   var BlockModel = require('core/models/blockModel');
   var ComponentModel = require('core/models/componentModel');
+  var ContentObjectModel = require('core/models/contentObjectModel');
+  var EditorMenuView = require('../../contentObject/views/editorMenuView');
+  var EditorOriginView = require('./editorOriginView');
+  var EditorPageView = require('../../contentObject/views/editorPageView');
+  var helpers = require('core/helpers');
+  var Origin = require('core/origin');
 
   var EditorView = EditorOriginView.extend({
     className: "editor-view",
