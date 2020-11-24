b3254e5c0f360503b67b2472872745379e15f592
Tom Taylor <hello@tomtaylor.name>
2020-11-24 13:07:15 +0000

    Remove unnecessary vars for readability

--- app/modules/editor/contentObject/views/editorPageComponentPasteZoneView.js
+++ app/modules/editor/contentObject/views/editorPageComponentPasteZoneView.js
@@ -22,36 +22,17 @@ define(function(require){
         accept: () =>  el.hasClass('component-draggable') && $(this).css('visibility') == 'visible',
         hoverClass: 'paste-zone-droppable',
         drop: function (e, ui) {
-          var $component = $(ui.draggable);
-          var parentId = view.model.get('_parentId');
-          var blockId = $component.attr('data-block-id');
-          var componentId = $component.attr('data-component-id');
-          var left = $(this).hasClass('paste-zone-component-left');
-          var right = $(this).hasClass('paste-zone-component-right');
-          var newLayout = (!left && !right) ? 'full' : (left ? 'left' : 'right');
-          var newData = {
-            _layout: newLayout,
-            _parentId: parentId
-          };
-
+          var isLeft = $(this).hasClass('paste-zone-component-left');
+          var isRight = $(this).hasClass('paste-zone-component-right');
           $.ajax({
             type: 'PUT',
-            url:'api/content/component/' + componentId,
-            data: newData,
-            success: function(jqXHR, textStatus, errorThrown) {
-              // Re-render the move-from block
-              Origin.trigger('editorView:moveComponent:' + blockId);
-              if (blockId !== parentId) {
-                // Re-render the move-to block
-                Origin.trigger('editorView:moveComponent:' + parentId);
-              }
+            url: `api/content/component/${$(ui.draggable).attr('data-component-id')}`,
+            data: {
+              _layout: (!isLeft && !isRight) ? 'full' : (isLeft ? 'left' : 'right'),
+              _parentId: view.model.get('_parentId')
             },
-            error: function(jqXHR, textStatus, errorThrown) {
-              Origin.Notify.alert({
-                type: 'error',
-                text: jqXHR.responseJSON.message
-              });
-            }
+            success: () => Origin.trigger('editorView:renderPage'),
+            error: ({ responseJSON }) => Origin.Notify.alert({ type: 'error', text: responseJSON.message })
           });
         }
       });
