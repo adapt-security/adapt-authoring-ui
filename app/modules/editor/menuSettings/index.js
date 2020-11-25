8148aceb707456167c5930765fe43bd8d86d825e
Tom Taylor <hello@tomtaylor.name>
2020-11-25 21:09:20 +0000

    Refactor to remove model fetch

--- app/modules/editor/menuSettings/index.js
+++ app/modules/editor/menuSettings/index.js
@@ -1,30 +1,27 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Origin = require('core/origin');
-
-  var ConfigModel = require('core/models/configModel');
-  var EditorMenuSettingsEditView = require('./views/editorMenuSettingsEditView');
   var EditorMenuSettingsEditSidebarView = require('./views/editorMenuSettingsEditSidebarView');
+  var EditorMenuSettingsEditView = require('./views/editorMenuSettingsEditView');
   var Helpers = require('../global/helpers');
+  var Origin = require('core/origin');
 
   Origin.on('editor:menusettings', function(data) {
     var route1 = Origin.location.route1;
-    (new ConfigModel({ _courseId: route1 })).fetch({
-      success: function(model) {
-        Helpers.setPageTitle(model);
-
-        var backButtonRoute = "#/editor/" + route1 + "/menu";
-        var backButtonText = Origin.l10n.t('app.backtomenu');
-        if (Origin.previousLocation.route2 === "page") {
-          backButtonRoute = "#/editor/" + route1 + "/page/" + Origin.previousLocation.route3;
-          backButtonText = Origin.l10n.t('app.backtopage');
-        }
-        Origin.sidebar.addView(new EditorMenuSettingsEditSidebarView().$el, {
-          "backButtonText": backButtonText,
-          "backButtonRoute": backButtonRoute
-        });
-        Origin.contentPane.setView(EditorMenuSettingsEditView, { model: model });
-      }
+    var model = Origin.editor.data.config;
+    
+    Helpers.setPageTitle(model);
+    
+    var backButtonRoute = `#/editor/${route1}/menu`;
+    var backButtonText = Origin.l10n.t('app.backtomenu');
+    
+    if (Origin.previousLocation.route2 === "page") {
+      backButtonRoute = `#/editor/${route1}/page/${Origin.previousLocation.route3}`;
+      backButtonText = Origin.l10n.t('app.backtopage');
+    }
+    Origin.sidebar.addView(new EditorMenuSettingsEditSidebarView().$el, {
+      backButtonText,
+      backButtonRoute
     });
+    Origin.contentPane.setView(EditorMenuSettingsEditView, { model });
   });
 });
