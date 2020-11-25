92a2ac683b4755a1a7ce18f8668b0af126fb11b9
Tom Taylor <hello@tomtaylor.name>
2020-11-25 21:02:47 +0000

    Refactor to remove model fetch

--- app/modules/editor/themeEditor/index.js
+++ app/modules/editor/themeEditor/index.js
@@ -1,7 +1,6 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
   var Origin = require('core/origin');
-  var EditorConfigModel = require('core/models/configModel');
   var EditorThemingView = require('./views/editorThemingView.js');
   var EditorThemingSidebarView = require('./views/editorThemingSidebarView.js');
 
@@ -9,12 +8,8 @@ define(function(require) {
     Origin.router.navigate('#/editor/' + Origin.editor.data.course.get('_id') + '/' + ROUTE, { trigger: true });
   });
 
-  Origin.on('editor:selecttheme', function(route1, route2, route3, route4) {
-    Origin.editor.data.config.fetch({
-      success: function(configModel) {
-        Origin.sidebar.addView(new EditorThemingSidebarView().$el);
-        Origin.contentPane.setView(EditorThemingView, { model: configModel });
-      }
-    });
+  Origin.on('editor:selecttheme', () => {
+    Origin.sidebar.addView(new EditorThemingSidebarView().$el);
+    Origin.contentPane.setView(EditorThemingView, { model: Origin.editor.data.config });
   });
 });
