79fbff5ca69355423123e77ee1f641bfc03d96c1
Tom Taylor <hello@tomtaylor.name>
2020-11-26 11:26:16 +0000

    Refactor to extend ApiCollection

--- app/core/collections/tagsCollection.js
+++ app/core/collections/tagsCollection.js
@@ -1,5 +1,4 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
-define(function(require) {
-	var Backbone = require('backbone');
-	return Backbone.Collection.extend({ url: 'api/tags' });
+define(['core/collections/apiCollection'], function(ApiCollection) {
+	return ApiCollection.extend({ url: 'api/tags' });
 });
