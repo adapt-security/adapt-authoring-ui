a17ec32e8ef3fb8075d3a95ca32d6e738a51d33c
Tom Taylor <hello@tomtaylor.name>
2020-11-26 12:05:00 +0000

    Remove unused import

--- app/modules/editor/global/views/editorOriginView.js
+++ app/modules/editor/global/views/editorOriginView.js
@@ -1,6 +1,5 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require){
-  var ContentCollection = require('core/collections/contentCollection');
   var Origin = require('core/origin');
   var OriginView = require('core/views/originView');
 
