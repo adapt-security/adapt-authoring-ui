1c9e328f3925051614ce37bab7de866dd7ec8147
Tom Taylor <hello@tomtaylor.name>
2021-01-11 17:46:12 +0000

    Add attribute

--- app/core/collections/apiCollection.js
+++ app/core/collections/apiCollection.js
@@ -1,6 +1,8 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(['backbone', 'underscore'], function(Backbone, _) {
   var ApiCollection = Backbone.Collection.extend({
+    options: {},
+    
     initialize : function(models, options) {
       Backbone.Collection.prototype.initialize.apply(this, arguments);
       if(!options) options = {};
