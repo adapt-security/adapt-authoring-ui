7b64085ff10745f1718847b416f1399ab18b9326
Tom Taylor <hello@tomtaylor.name>
2021-03-17 14:49:34 +0000

    Remove unused import

--- app/core/models/courseAssetModel.js
+++ app/core/models/courseAssetModel.js
@@ -1,7 +1,6 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
   var Backbone = require('backbone');
-  var Origin = require('core/origin');
 
   var CourseAssetModel = Backbone.Model.extend({
     idAttribute: '_id',
