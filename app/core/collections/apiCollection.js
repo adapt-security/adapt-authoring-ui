58c7820fc54809d9633a6c65725b485536423f38
Thomas Taylor <thomas.taylor@kineo.com>
2022-04-28 12:25:13 +0100

    Update API collection for paging params

--- app/core/collections/apiCollection.js
+++ app/core/collections/apiCollection.js
@@ -15,12 +15,27 @@ define(['backbone', 'underscore'], function(Backbone, _) {
     buildQueryParams: function() {
       return _.isEmpty(this.options) ? '' : Object.entries(this.options).reduce((q,[k,v]) => `${q}${k}=${JSON.stringify(v)}&`, '?');
     },
-    fetch: function(options) {
-      return Backbone.Collection.prototype.fetch.call(this, _.assign({
-        url: `${this.url}/query${this.buildQueryParams()}`,
-        method: 'POST',
-        data: this.buildQuery()
-      }, options));
+    fetch: async function(options = {}) {
+      const _fetch = (url, memo = []) => {
+        return new Promise((resolve, reject) => {
+          Backbone.Collection.prototype.fetch.call(this, _.assign({
+            url,
+            method: 'POST',
+            data: this.buildQuery(),
+            success: async (d, status, res) => {
+              memo.push(...d.models);
+              const link = res.xhr.getResponseHeader('Link');
+              if(link) {
+                const nextUrl = link.match(/<(.+)>; rel="next",/)[1];
+                if(nextUrl) return resolve(_fetch(nextUrl, memo));
+              }
+              resolve(memo);
+            }, 
+            error: console.log
+          }, options));
+        });
+      };
+      this.reset(await _fetch(`${this.url}/query${this.buildQueryParams()}`));
     }
   });
 
