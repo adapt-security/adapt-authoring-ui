54cbf141185cfb0e3f72dfe0045e54567b7394dd
Thomas Taylor <thomas.taylor@kineo.com>
2022-05-04 23:04:48 +0100

    Update doc plugin

--- docs/plugins/uidocs.js
+++ docs/plugins/uidocs.js
@@ -1,88 +1,88 @@
 import { exec } from 'child_process';
-import { fileURLToPath } from 'url';
+import { fileURLToPath, pathToFileURL } from 'url';
 import fs from 'fs-extra';
 import glob from 'glob';
+import path from 'path';
 import { promisify } from 'util';
 
 const execPromise = promisify(exec);
 
-const configPath = resolvePath('.jsdocConfig.json');
-let docsRootPath;
 
-let cachedConfigs;
+export default class UIDocs {
+  constructor(app, config) {
+    this.app = app;
+    this.outputDir = path.join(config.outputDir, 'ui');
+    this.version = config.version;
+    this.docsRootPath = path.join(this.app.dependencies['adapt-authoring-docs'].rootDir, 'jsdoc3') + path.sep;
+    this.configPath = '';
+  }
+  resolvePath(relativePath, basePath) {
+    if(!basePath) basePath = this.docsRootPath;
+    if(typeof basePath === 'string') basePath = pathToFileURL(basePath);
+    relativePath = relativePath.replaceAll('/', path.sep);
+    return fileURLToPath(new URL(relativePath, basePath));
+  }
+  async run() {
+    this.configPath = this.resolvePath('.jsdocConfig.json', new URL(import.meta.url));
+    await this.writeConfig();
 
-function resolvePath(relativePath) {
-  return fileURLToPath(new URL(relativePath, import.meta.url));
-}
-
-async function writeConfig(app, outputdir, indexFile) {
-  const version = app.pkg.version;
-  return fs.writeFile(configPath, JSON.stringify({
-    "source": { 
-      "include": getSourceIncludes(indexFile) 
-    },
-    "docdash": {
-      "collapse": true,
-      "typedefs": true,
-      "search": false,
-      "static": true,
-      "menu": {
-        [`<img class="logo" src="assets/logo-colour.png" />Adapt authoring tool UI documentation<br><span class="version">v${version}</span>`]: {
-          "class":"menu-title"
-        },
-        "Home": {
-          "href":"index.html",
-          "target":"_self",
-          "class":"menu-item",
-          "id":"home_link"
-        },
-        "Project Website": {
-          "href":"https://www.adaptlearning.org/",
-          "target":"_blank",
-          "class":"menu-item",
-          "id":"website_link"
-        },
-        "Technical Discussion Forum": {
-          "href":"https://community.adaptlearning.org/mod/forum/view.php?id=4",
-          "target":"_blank",
-          "class":"menu-item",
-          "id":"forum_link"
-        }
+    await execPromise(`npx jsdoc -c ${this.configPath}`);
+    await Promise.all([
+      fs.copy(this.resolvePath(`./styles/adapt.css`), `${this.outputDir}/styles/adapt.css`),
+      fs.copy(this.resolvePath(`./scripts/adapt.js`), `${this.outputDir}/scripts/adapt.js`),
+      fs.copy(this.resolvePath(`../assets`), `${this.outputDir}/assets`)
+    ]); 
+  }
+  async writeConfig() {
+    const version = this.app.dependencyloader.configs['adapt-authoring-docs'].version;
+    return fs.writeFile(this.configPath, JSON.stringify({
+      "source": { 
+        "include": [
+          this.resolvePath('index.md', new URL(import.meta.url)),
+          ...glob.sync(`${this.app.dependencies['adapt-authoring-ui'].rootDir}/app/**/*.js`, { absolute: true })
+        ].filter(f => !f.includes('/libraries/'))
       },
-      "meta": {
-        "title": "Adapt authoring tool UI documentation",
-        "keyword": `v${version}`
+      "docdash": {
+        "collapse": true,
+        "typedefs": true,
+        "search": false,
+        "static": true,
+        "menu": {
+          [`<img class="logo" src="assets/logo-colour.png" />Adapt authoring tool UI documentation<br><span class="version">v${version}</span>`]: {
+            "class":"menu-title"
+          },
+          "Home": {
+            "href":"index.html",
+            "target":"_self",
+            "class":"menu-item",
+            "id":"home_link"
+          },
+          "Project Website": {
+            "href":"https://www.adaptlearning.org/",
+            "target":"_blank",
+            "class":"menu-item",
+            "id":"website_link"
+          },
+          "Technical Discussion Forum": {
+            "href":"https://community.adaptlearning.org/mod/forum/view.php?id=4",
+            "target":"_blank",
+            "class":"menu-item",
+            "id":"forum_link"
+          }
+        },
+        "meta": {
+          "title": "Adapt authoring tool UI documentation",
+          "keyword": `v${version}`
+        },
+        "scripts": [
+          'styles/adapt.css',
+          'scripts/adapt.js'
+        ],
       },
-      "scripts": [
-        'styles/adapt.css',
-        'scripts/adapt.js'
-      ],
-    },
-    "opts": {
-      "destination": outputdir,
-      "template": "node_modules/docdash"
-    }
-  }, null, 2));
-}
-/**
- * Returns a list of modules to include.
- * @note Source files must be located in /lib
- */
-function getSourceIncludes(indexFile) {
-  const includes = cachedConfigs.reduce((i, c) => {
-    return i.concat(glob.sync('app/**/*.js', { cwd: c.rootDir, absolute: true }));
-  }, []);
-  if(indexFile) includes.push(indexFile);
-  return includes;
-}
-
-export default async function jsdoc3(app, configs, outputdir, sourceIndexFile) {
-  const dir = `${outputdir}/ui`;
-  await writeConfig(app, dir, sourceIndexFile);
-  await execPromise(`npx jsdoc -c ${configPath}`);
-  await Promise.all([
-    fs.copy(resolvePath(`./styles/adapt.css`), `${dir}/styles/adapt.css`),
-    fs.copy(resolvePath(`./scripts/adapt.js`), `${dir}/scripts/adapt.js`),
-    fs.copy(resolvePath(`../assets`), `${dir}/assets`)
-  ]);
+      "opts": {
+        "destination": this.outputDir,
+        "template": "node_modules/docdash"
+      }
+    }, null, 2));
+  }
 }
