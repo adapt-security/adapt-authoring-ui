37ce71d409a4980379b50d7af3ee59923cf13b46
Thomas Taylor <thomas.taylor@kineo.com>
2022-05-31 17:11:43 +0100

    Remove unused import

--- app/modules/user/views/userProfileView.js
+++ app/modules/user/views/userProfileView.js
@@ -1,7 +1,6 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require){
   var Backbone = require('backbone');
-  var Handlebars = require('handlebars');
   var OriginView = require('core/views/originView');
   var Origin = require('core/origin');
 
