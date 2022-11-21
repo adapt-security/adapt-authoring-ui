fd78dcb7a5f8a37344e81f744def9de00e35e0ac
Thomas Taylor <thomas.taylor@kineo.com>
2022-11-21 18:15:13 +0000

    Add config load to postinstall

--- npm_hooks/postinstall.js
+++ npm_hooks/postinstall.js
@@ -1,1 +1,8 @@
-console.log('UI postinstall', process.cwd());
+import path from 'path';
+import { pathToFileURL } from 'url';
+
+console.log('UI postinstall', path.resolve(`../../conf/${process.NODE_ENV}.config.js`));
+
+import(pathToFileURL(path.resolve(`../../conf/${process.NODE_ENV}.config.js`))).then(config => {
+  console.log(config);
+});
