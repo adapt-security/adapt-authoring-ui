2376572ae7d9bbabaaead633b3b28ab785dd0eda
Thomas Taylor <thomas.taylor@kineo.com>
2022-11-22 11:37:11 +0000

    Fix path in postinstall script

--- npm_hooks/postinstall.js
+++ npm_hooks/postinstall.js
@@ -1,5 +1,4 @@
 import fs from 'fs/promises';
-import path from 'path';
 
-fs.writeFile(path.resolve(__dirname, `../../.rebuild-ui`), ' ')
+fs.writeFile(new URL(`../../.rebuild-ui`, import.meta.url), ' ')
   .catch(console.log);
