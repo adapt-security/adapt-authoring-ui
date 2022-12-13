029fbc4edac800e2dfc787812f7667a5f116129f
Thomas Taylor <thomas.taylor@kineo.com>
2022-12-13 12:24:20 +0000

    Refactor GlobalMenu code

--- app/modules/globalMenu/index.js
+++ app/modules/globalMenu/index.js
@@ -4,7 +4,7 @@ define(function(require) {
     var GlobalMenuView = require('./views/globalMenuView');
 
     class GlobalMenu {
-        isActive = false;
+        isOpen = false;
         itemStore = new Backbone.Collection([
             {
                 location: "global",
@@ -17,20 +17,19 @@ define(function(require) {
         constructor() {
             this.itemStore.comparator = 'sortOrder';
             
+            Origin.on('login:changed', function() {
+                if(!Origin.sessionModel.get('isAuthenticated')) this.itemStore.remove(this.itemStore.models);
+            });
+            Origin.on('origin:dataReady', this.renderButton.bind(this));
             Origin.on('navigation:globalMenu:toggle',this.toggle.bind(this));
             Origin.on('remove:views globalMenu:close', this.close.bind(this));
-            // Listen to when the user is logger out and reset the collection as permissions
-            // can be carried over from different users logging into the same machine
-            Origin.on('login:changed', function() {
-                if (!Origin.sessionModel.get('isAuthenticated')) {
-                    GlobalMenuStore.remove(GlobalMenuStore.models);
-                }
-                this.renderButton();
-            });
         }
         renderButton() {
+            if(!this.itemStore.length) {
+                return;
+            }
             var $btn = $(Handlebars.partials.part_globalMenuButton());
-            $('.navigation .navigation-left').append($btn);
+            $('.navigation .navigation-left').prepend($btn);
             $btn.click(() => Origin.trigger('navigation:globalMenu:toggle'));
         }
         addItem(item, isSubItem) {
@@ -40,7 +39,7 @@ define(function(require) {
             }
             if (!item.hasOwnProperty('sortOrder')) item.sortOrder = 99;
             if(isSubItem) {
-                item._isSubMenuItem = true;
+                item.isSubItem = true;
                 if (!this.itemStore.findWhere({ text: item.parent })) {
                     return console.log("Cannot add Sub Menu item as there's no parentItem", item.text);
                 }
@@ -60,26 +59,21 @@ define(function(require) {
             });
         }
         open() {
-            this.isActive = true;
-            // Add new view to the .navigation element passing in the GlobalMenuStore as the collection
+            this.isOpen = true;
             $('.navigation').append(new GlobalMenuView({ collection: this.itemStore }).$el);
             // Setup listeners to #app to remove menu when main page is clicked
             // Cheeky little defer here to stop it creating a closing loop
-            _.defer(function() {
-                $('#app, .sidebar, .navigation').one('click', _.bind(function(event) {
-                    Origin.trigger('globalMenu:close');
-                }, this));
-            });
+            _.defer(() => $('#app, .sidebar, .navigation').one('click', () => Origin.trigger('globalMenu:close')));
         }
         close() {
             $('#app, .sidebar').off('click');
-            this.isActive = false;
+            this.isOpen = false;
             // Trigger event to remove the globalMenuView
             Origin.trigger('globalMenu:globalMenuView:remove');
             $('#global-menu-icon').removeClass('open');
         }
         toggle() {
-            this.isActive ? this.close() : this.open();
+            this.isOpen ? this.close() : this.open();
         }
     };
 
--- app/modules/globalMenu/views/globalMenuView.js
+++ app/modules/globalMenu/views/globalMenuView.js
@@ -1,11 +1,9 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-
     var Origin = require('core/origin');
     var GlobalMenuItemView = require('./globalMenuItemView');
 
     var GlobalMenuView = Backbone.View.extend({
-
         className: 'global-menu',
 
         initialize: function() {
@@ -16,48 +14,30 @@ define(function(require) {
         render: function() {
             var template = Handlebars.templates[this.constructor.template];
             this.$el.html(template());
-            _.defer(_.bind(function() {
-                this.postRender();
-            }, this));
+            _.defer(() => this.postRender());
             return this;
         },
 
         postRender: function() {
+            this.collection.each(this.renderItem, this);
+            _.defer(() => this.$el.fadeIn(300));
+        },
 
-            this.collection.each(function(menuItem) {
-
-                var location = menuItem.get('location');
-                var _isSubItem = menuItem.get('_isSubMenuItem');
-                // Check location and only render if location is either global or current location
-                if (location === 'global' || location === Origin.location.module) {
-                    // Only load view if it's not a subItem
-                    if (_isSubItem === false) {
-                        this.$('.global-menu-inner').append(new GlobalMenuItemView({
-                            collection: this.collection,
-                            model: menuItem
-                        }).$el);
-                    }
-                }
-
-            }, this);
-
-            // Wait until menu items are rendered before showing
-            _.defer(_.bind(function() {
-                this.$el.fadeIn(300);
-            }, this));
-
+        renderItem(item) {
+            var location = item.get('location');
+            var isSubItem = item.get('isSubItem');
+            if((location !== 'global' && location !== Origin.location.module) || isSubItem) {
+                return;
+            }
+            this.$('.global-menu-inner').append(new GlobalMenuItemView({ collection: this.collection, model: item }).$el);
         },
 
         removeMenu: function() {
-            this.$el.fadeOut(300, _.bind(function() {
-                this.remove();
-            }, this));
+            this.$el.fadeOut(300, () => this.remove());
         }
-
     }, {
         template: 'globalMenu'
     });
 
     return GlobalMenuView;
-
 })
