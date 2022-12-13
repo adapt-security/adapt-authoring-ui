658334c87b7db50874be05195d5c57677c34f6d6
Thomas Taylor <thomas.taylor@kineo.com>
2022-12-13 11:50:11 +0000

    Rewrite GlobalMenu code

--- app/modules/globalMenu/index.js
+++ app/modules/globalMenu/index.js
@@ -1,112 +1,87 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-
     var Origin = require('core/origin');
     var GlobalMenuView = require('./views/globalMenuView');
 
-    // Create GlobalMenu object
-    var GlobalMenu = {};
-    var _isActive = false;
-    // Create GlobalMenu Store
-    var GlobalMenuStore = new Backbone.Collection([
-        {
-            location: "global",
-            text: Origin.l10n.t('app.dashboard'),
-            icon: "fa-home",
-            callback: Origin.router.navigateToDashboard,
-            sortOrder: 1
+    class GlobalMenu {
+        isActive = false;
+        itemStore = new Backbone.Collection([
+            {
+                location: "global",
+                text: Origin.l10n.t('app.dashboard'),
+                icon: "fa-home",
+                callback: () => Origin.router.navigateToDashboard(),
+                sortOrder: 1
+            }
+        ]);
+        constructor() {
+            this.itemStore.comparator = 'sortOrder';
+            
+            Origin.on('navigation:globalMenu:toggle',this.toggle.bind(this));
+            Origin.on('remove:views globalMenu:close', this.close.bind(this));
+            // Listen to when the user is logger out and reset the collection as permissions
+            // can be carried over from different users logging into the same machine
+            Origin.on('login:changed', function() {
+                if (!Origin.sessionModel.get('isAuthenticated')) {
+                    GlobalMenuStore.remove(GlobalMenuStore.models);
+                }
+                this.renderButton();
+            });
         }
-    ]);
-    GlobalMenuStore.comparator = 'sortOrder';
-
-    // Method for adding and item to the global menu
-    GlobalMenu.addItem = function(itemObject) {
-        // Return if itemObject doesn't have all arguments
-        if (_.size(itemObject) < 4) {
-            return console.log('Cannot add Global Menu item ', itemObject.text);
+        renderButton() {
+            var $btn = $(Handlebars.partials.part_globalMenuButton());
+            $('.navigation .navigation-left').append($btn);
+            $btn.click(() => Origin.trigger('navigation:globalMenu:toggle'));
         }
-
-        if (!itemObject.hasOwnProperty('sortOrder')) {
-            itemObject.sortOrder = 99;
+        addItem(item, isSubItem) {
+            const isValid = this.validateItem(item, isSubItem);
+            if(!isValid) {
+                return;
+            }
+            if (!item.hasOwnProperty('sortOrder')) item.sortOrder = 99;
+            if(isSubItem) {
+                item._isSubMenuItem = true;
+                if (!this.itemStore.findWhere({ text: item.parent })) {
+                    return console.log("Cannot add Sub Menu item as there's no parentItem", item.text);
+                }
+            }
+            if (!this.itemStore.findWhere({ text: item.text })) this.itemStore.add(item);
         }
-
-        itemObject._isSubMenuItem = false;
-        if (!GlobalMenuStore.findWhere({text: itemObject.text})) {
-            // Push item to GlobalMenuStore
-            GlobalMenuStore.add(itemObject);
+        validateItem(item, isSubItem = false) {
+            const hasAttributes = (...keys) => keys.all(k => item.hasOwnProperty(k));
+            const keys = ['location', 'text', 'icon', ['callback', 'callbackEvent']];
+            if(isSubItem) keys.push('parent');
+            keys.forEach(attr => {
+                const errorMsg = 'Cannot add global menu item,';
+                if(Array.isArray(attr)) {
+                    if(!hasAttributes(...attr)) return console.log(errorMsg, `must define one of: ${attr.join(', ')}`);
+                }
+                if(!hasAttributes(attr)) return console.log(errorMsg, `missing ${attr}`);
+            });
         }
-
-    }
-
-    // Method for adding a sub item to the global menu
-    GlobalMenu.addSubItem = function(subItemObject) {
-        // Return if subItemObject doesn't have all arguments
-        if (_.size(subItemObject) !== 5) {
-            return console.log('Cannot add Sub Menu item', subItemObject.text);
+        open() {
+            this.isActive = true;
+            // Add new view to the .navigation element passing in the GlobalMenuStore as the collection
+            $('.navigation').append(new GlobalMenuView({ collection: this.itemStore }).$el);
+            // Setup listeners to #app to remove menu when main page is clicked
+            // Cheeky little defer here to stop it creating a closing loop
+            _.defer(function() {
+                $('#app, .sidebar, .navigation').one('click', _.bind(function(event) {
+                    Origin.trigger('globalMenu:close');
+                }, this));
+            });
         }
-
-        // Get parentItem
-        var parentItem = GlobalMenuStore.findWhere({text: subItemObject.parent});
-
-        // Check parentItem exists and there's only one
-        if (!parentItem) {
-            return console.log("Cannot add Sub Menu item as there's no parentItem", subItemObject.text);
+        close() {
+            $('#app, .sidebar').off('click');
+            this.isActive = false;
+            // Trigger event to remove the globalMenuView
+            Origin.trigger('globalMenu:globalMenuView:remove');
+            $('#global-menu-icon').removeClass('open');
         }
-
-        subItemObject._isSubMenuItem = true;
-        // Push item to GlobalMenuStore
-        GlobalMenuStore.add(subItemObject);
-    }
-
-    // Listen to navigation event to toggle
-    Origin.on('navigation:globalMenu:toggle', function() {
-        // Remove all events off #app
-        $('#global-menu-icon').toggleClass('open');
-        $('#app, .sidebar').off('click');
-        // Toggle between displaying and removing the menu
-        if (_isActive === true) {
-            closeGlobalMenu();
-        } else {
-            openGlobalMenu();
+        toggle() {
+            this.isActive ? this.close() : this.open();
         }
-    });
+    };
 
-    Origin.on('remove:views globalMenu:close', function() {
-        $('#app, .sidebar').off('click');
-        closeGlobalMenu();
-        $('#global-menu-icon').removeClass('open');
-    });
-
-    // Listen to when the user is logger out and reset the collection as permissions
-    // can be carried over from different users logging into the same machine
-    Origin.on('login:changed', function() {
-        if (!Origin.sessionModel.get('isAuthenticated')) {
-            GlobalMenuStore.remove(GlobalMenuStore.models);
-        }
-    });
-
-    function openGlobalMenu() {
-        _isActive = true;
-
-        // Add new view to the .navigation element passing in the GlobalMenuStore as the collection
-        $('.navigation').append(new GlobalMenuView({collection: GlobalMenuStore}).$el);
-        // Setup listeners to #app to remove menu when main page is clicked
-        // Cheeky little defer here to stop it creating a closing loop
-        _.defer(function() {
-            $('#app, .sidebar, .navigation').one('click', _.bind(function(event) {
-                Origin.trigger('globalMenu:close');
-            }, this));
-        });
-
-    }
-
-    function closeGlobalMenu() {
-        _isActive = false;
-        // Trigger event to remove the globalMenuView
-        Origin.trigger('globalMenu:globalMenuView:remove');
-        // Remove body click event
-    }
-
-    Origin.globalMenu = GlobalMenu;
-
+    Origin.globalMenu = new GlobalMenu();
 });
