0b5819d16f7a331808b8af11bbc84c0bf4f3bb2b
Thomas Taylor <thomas.taylor@kineo.com>
2022-12-16 14:32:47 +0000

    Update ContentHeaderButtonView

--- app/modules/contentHeader/views/actionsButtonView.js
+++ app/modules/contentHeader/views/actionsButtonView.js
@@ -1,15 +1,10 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Origin = require('core/origin');
   var ContentHeaderButtonView = require('./contentHeaderButtonView');
 
   var ActionsButtonView = ContentHeaderButtonView.extend({
-    preRender: function() {
+    async preRender() {
       if(!this.data.buttonClass) this.data.buttonClass = 'action-primary';
-    },
-
-    getEventName($targetEl) {
-      return `actions:${this.data.eventName}`;
     }
   }, {
     template: 'actionsButton'
--- app/modules/contentHeader/views/contentHeaderButtonView.js
+++ app/modules/contentHeader/views/contentHeaderButtonView.js
@@ -8,29 +8,36 @@ define(function(require) {
     tagName: 'span',
 
     events: {
-    'click button': 'onClicked'
+      'click button': 'onClicked'
     },
 
-    initialize: function(options) {
+    async initialize(options) {
       this.listenTo(Origin, 'remove:views', this.remove);
 
       this.data = options;
       if(!this.data.buttonClass) this.data.buttonClass = 'action-primary';
+      try {
+        await this.preRender();
+      } catch(e) {
+        return Origin.Notift.alert({ type: 'error', text: e.toString() });
+      }
       this.render();
+      setTimeout(this.postRender.bind(this), 0);
     },
-
-    render: function() {
+    
+    async preRender() {},
+    
+    render() {
       this.$el.html(Handlebars.templates[this.constructor.template](this.data));
       return this;
     },
 
-    getEventName: function() {
-      return this.data.eventName;
-    },
+    postRender() {},
 
-    onClicked: function(event) {
-      event && event.preventDefault();
-      Origin.trigger(this.getEventName($(event.currentTarget)));
+    onClicked(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      Origin.trigger(`${this.data.type}:${this.data.eventName}`);
     }
   });
 
