9885bc187f36741e65e5292a12608876e5fe26a7
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-04 17:34:22 +0000

    Implement sort button icons

--- app/modules/contentHeader/views/sortsButtonView.js
+++ app/modules/contentHeader/views/sortsButtonView.js
@@ -4,17 +4,33 @@ define(function(require) {
   var ContentHeaderToggleButtonView = require('./contentHeaderToggleButtonView');
   
   var SortsButtonView = ContentHeaderToggleButtonView.extend({
-    toggleSortDirection() {
-      this.direction = this.direction === -1 ? 1 : -1;
-      this.$('.item').removeClass('selected');
-      this.$('item input[type="radio"]:selected').parent('.item').$('i.asc').toggle(this.direction === 1);
-      this.$('item input[type="radio"]:selected').parent('.item').$('i.desc').toggle(this.direction === -1);
+    render() {
+      ContentHeaderToggleButtonView.prototype.render.apply(this);
+      this.updateIcon();
+    },
+    updateIcon() {
+      this.$('.item').each((i, item) => {
+        var $i = $(item);
+        $('i', $i).hide();
+        if(!$i.hasClass('selected') || this.direction === undefined) {
+          $('i.default', $i).show();
+        } else {
+          $(`i.${this.direction === 1 ? 'asc' : 'desc'}`, $i).show();
+        }
+      });
     },
     onItemClicked(event) {
+      event.preventDefault();
+      event.stopPropagation();
       const $selected = $(event.currentTarget);
-      this.$('.item').removeClass('selected');
-      $selected.addClass('selected');
-      this.toggleSortDirection();
+      if(!$selected.hasClass('selected')) {
+        this.direction = 1;
+        this.$('.item').removeClass('selected');
+        $selected.addClass('selected');
+      } else {
+        this.direction = this.direction === 1 ? -1 : 1;
+      }
+      this.updateIcon();
 
       const eventName = `${this.data.type}:${$selected.attr('data-event')}`;
       Origin.trigger(eventName, this.direction);
