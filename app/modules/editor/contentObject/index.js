1d5c4e72ae707f693e3868ac5adb04cec60e3da8
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-06 23:35:34 +0000

    Refactor for readability

--- app/modules/editor/contentObject/index.js
+++ app/modules/editor/contentObject/index.js
@@ -1,59 +1,16 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
-/**
- * This module handles both sections/menus and pages.
- */
 define(function(require) {
-  var EditorMenuSidebarView = require('./views/editorMenuSidebarView');
-  var EditorPageSidebarView = require('./views/editorPageSidebarView');
   var EditorView = require('../global/views/editorView');
   var Helpers = require('../global/helpers');
   var Origin = require('core/origin');
 
   Origin.on('editor:contentObject', function(data) {
-    var route = function() {
-      if(data.action === 'edit') {
-        return renderContentObjectEdit(data);
-      }
-      if(data.id) {
-        return renderPageStructure(data);
-      } 
-      renderMenuStructure(data);
-    }
-    if(!data.id) {
-      return route();
-    }
-    data.model = Origin.editor.data.content.findWhere({ _id: data.id });
-    Helpers.setPageTitle(data.model);
-    route();
+    const model = data.id ? Origin.editor.data.content.findWhere({ _id: data.id }) : Origin.editor.data.course;
+    Helpers.setPageTitle(model);
+    Origin.contentPane.setView(EditorView, {
+      currentCourseId: Origin.location.route1,
+      currentView: data.type,
+      currentPageId: data.id
+    });
   });
-
-  function renderPageStructure(data) {
-    Helpers.setPageTitle(data.model);
-
-    Origin.sidebar.addView(new EditorPageSidebarView().$el, {
-      backButtonText: Origin.l10n.t('app.backtomenu'),
-      backButtonRoute: "#/editor/" + Origin.location.route1 + "/menu"
-    });
-    Origin.contentPane.setView(EditorView, {
-      currentCourseId: Origin.location.route1,
-      currentView: 'page',
-      currentPageId: data.id
-    });
-  }
-
-  function renderMenuStructure(data) {
-    Helpers.setPageTitle(Origin.editor.data.course);
-
-    Origin.editor.scrollTo = 0;
-
-    Origin.sidebar.addView(new EditorMenuSidebarView().$el, {
-      backButtonText: Origin.l10n.t('app.backtoprojects'),
-      backButtonRoute: '#/projects'
-    });
-    Origin.contentPane.setView(EditorView, {
-      currentCourseId: Origin.location.route1,
-      currentView: 'menu',
-      currentPageId: data.id
-    });
-  }
 });
