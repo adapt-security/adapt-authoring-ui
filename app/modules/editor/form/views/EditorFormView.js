687a6306750f6f99141e08d5f2d89d72de4e08a8
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-06 15:47:51 +0000

    Update EditorFormView

--- app/modules/editor/form/views/EditorFormView.js
+++ app/modules/editor/form/views/EditorFormView.js
@@ -2,30 +2,29 @@
 define(function(require) {
   var Origin = require('core/origin');
   var OriginView = require('core/views/originView');
-  var Helpers = require('../global/helpers');
+  var Helpers = require('../../global/helpers');
 
-  var EditorFormView = EditorOriginView.extend({
+  var EditorFormView = OriginView.extend({
     className: "ceditor-form",
     tagName: "div",
 
     initialize: function(options) {
-      if (options.model) {
-        Origin.scaffold.buildForm({ model: options.model })
-          .then(form => {
-            this.form = form;
-            this.filters = [];
-            this.listenTo(Origin, {
-              'actions:save': this.save,
-              'actions:cancel': this.cancel,
-              'editorSidebarView:removeEditView': this.remove
-            });
-            Helpers.setPageTitle(options.model);
-            Origin.trigger('sidebar:sidebarContainer:hide');
-            Origin.contentHeader.setButtons(Origin.contentHeader.BUTTON_TYPES.ACTIONS, Origin.contentHeader.ACTION_BUTTON_TEMPLATES.EDIT_FORM);
-            OriginView.prototype.initialize.apply(this, arguments);
-          })
-          .catch(e => Origin.Notify.alert({ type: 'error', text: e.message }));
-      }
+      Origin.scaffold.buildForm({ model: options.model })
+        .then(form => {
+          this.form = form;
+          this.filters = [];
+          this.listenTo(Origin, {
+            'actions:save': this.save,
+            'actions:cancel': Origin.router.navigateBack,
+            'editorSidebarView:removeEditView': this.remove
+          });
+          Helpers.setPageTitle(options.model);
+          Origin.trigger('sidebar:sidebarContainer:hide');
+          Origin.contentHeader.setButtons(Origin.contentHeader.BUTTON_TYPES.ACTIONS, Origin.contentHeader.ACTION_BUTTON_TEMPLATES.EDIT_FORM);
+
+          OriginView.prototype.initialize.apply(this, arguments);
+        })
+        .catch(e => Origin.Notify.alert({ type: 'error', text: e.message }));
     },
 
     render: function() {
@@ -35,38 +34,61 @@ define(function(require) {
     },
 
     postRender: function() {
-      if (!this.form) {
-        return this.setViewToReady();
-      }
+      this.setUpFilters();
       this.$('.form-container').append(this.form.el);
     },
 
-    filterForm: function(filter) {
-      if(this.filters.includes(filter)) { // toggle filter
-        this.filters = _.reject(this.filters, filterItem => filterItem === filter);
-      } else {
-        this.filters.push(filter);
+    setUpFilters() {
+      const filters = [{
+        items: [{
+          type: 'toggle',
+          buttonText: 'Hide non-text settings',
+          eventName: 'hidenontext'
+        }]
+      }];
+      const fieldsetFilters = [];
+      $('fieldset', this.form.el).each((i, fieldset) => {
+        const $f = $(fieldset);
+        const key = $f.attr('data-key');
+        if(!key) {
+          return;
+        }
+        fieldsetFilters.push({
+          type: 'toggle',
+          buttonText: $('legend', $f).text(),
+          checked: true,
+          eventName: 'fieldset'
+        });
+      });
+      Origin.on('filters:hidenontext', this.filterNonText);
+      if(fieldsetFilters.length) {
+        filters.push({
+          id: 'fieldsets',
+          name: 'Fieldsets',
+          items: fieldsetFilters
+        });
+        Origin.on('filters:fieldset', this.filterFieldsets);
       }
-      if(this.filters.length) { // Now actually filter the form
-        $('.form-container > form > div > fieldset').addClass('display-none');
-        this.filters.forEach(f => $(`fieldset[data-key=${f}]`).removeClass('display-none'));
-      } else {
-        $('.form-container > form > div > fieldset').removeClass('display-none');
-      }
+      Origin.contentHeader.setButtons(Origin.contentHeader.BUTTON_TYPES.FILTERS, filters);
+    },
+
+    
+    filterByTitle: function(filter) {
+      console.log('filterByTitle:', filter);
+    },
+    
+    filterNonText: function(filter) {
+      console.log('filterNonText:', filter);
+    },
+    
+    filterFieldsets: function(filter) {
+      console.log('filterFieldsets:', filter);
     },
 
     save: function() {
-      if(!this.form) {
-        return;
-      }
       var errors = this.form.validate();
-      // MUST trigger as sidebar needs to know when the form has been resubmitted
-      Origin.trigger('editorSidebar:showErrors', errors);
-
       if(errors) {
-        var errorText = `${Origin.l10n.t('app.validationfailedmessage')}<br/><br/>${this.buildErrorMessage(errors)}`;
-        // TODO remove when we've got a better solution
-        this.onSaveError(Origin.l10n.t('app.validationfailed'), errorText);
+        this.onSaveError(Origin.l10n.t('app.validationfailed'), this.buildErrorMessage(errors));
         return;
       }
       this.form.commit();
@@ -82,7 +104,7 @@ define(function(require) {
       });
     },
 
-    buildErrorMessage: function(errorObjs, message = "") {
+    buildErrorMessage: function(errorObjs, message = `${Origin.l10n.t('app.validationfailedmessage')}<br/><br/>`) {
       _.each(errorObjs, function(item, key) {
         if(item.hasOwnProperty('message')) {
           message += `<span class="key">${item.title || key}</span>: ${item.message}<br/>`;
@@ -111,7 +133,6 @@ define(function(require) {
         title: _.isString(pTitle) ? pTitle : Origin.l10n.t('app.errordefaulttitle'), 
         text: _.isString(pText) ? pText : Origin.l10n.t('app.errorsave')
       });
-      Origin.trigger('sidebar:resetButtons');
     }
   }, {
     template: 'editorForm'
