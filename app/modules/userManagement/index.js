457d5964e478174cf3c4153b85ed3bda8992ebfe
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-07 01:38:53 +0000

    Fix options attribute name

--- app/modules/userManagement/index.js
+++ app/modules/userManagement/index.js
@@ -99,7 +99,7 @@ define(function(require) {
     }]);
     Origin.trigger('contentHeader:updateTitle', { breadcrumbs: [{ title: Origin.l10n.t('app.usermanagement'), url: '#' }], title: Origin.l10n.t('app.usermanagementtitle') });
 
-    Origin.contentPane.setView(UserManagementView, { model, collection: userCollection }, { fullView: true });
+    Origin.contentPane.setView(UserManagementView, { model, collection: userCollection }, { fullWidth: true });
 
     Origin.on('actions:adduser', () => Origin.router.navigateTo('userManagement/addUser'));
     
