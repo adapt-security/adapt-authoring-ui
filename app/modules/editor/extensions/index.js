4e704bc8d11e31cae9ea8a80a7621eddbea6f48a
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-07 00:24:35 +0000

    Remove further sidebar references

--- app/modules/editor/extensions/index.js
+++ app/modules/editor/extensions/index.js
@@ -1,7 +1,6 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
   var Backbone = require('backbone');
-  var EditorExtensionsEditSidebarView = require('./views/editorExtensionsEditSidebarView');
   var EditorExtensionsEditView = require('./views/editorExtensionsEditView');
   var Origin = require('core/origin');
 
@@ -9,17 +8,6 @@ define(function(require) {
     Origin.trigger('contentHeader:updateTitle', {
       breadcrumbs: ['dashboard', 'course', { title: Origin.l10n.t('app.editorextensions') }],
       title: Origin.editor.data.course.get('title')
-    });
-    // Check whether the user came from the page editor or menu editor
-    var backButtonRoute = `#/editor/${Origin.location.route1}/menu`;
-    var backButtonText = Origin.l10n.t('app.backtomenu');
-    if (Origin.previousLocation.route2 === "page") {
-      backButtonRoute = `#/editor/${Origin.location.route1}/page/${Origin.previousLocation.route3}`;
-      backButtonText = Origin.l10n.t('app.backtopage');
-    }
-    Origin.sidebar.addView(new EditorExtensionsEditSidebarView().$el, {
-      backButtonText: backButtonText,
-      backButtonRoute: backButtonRoute
     });
     Origin.contentPane.setView(EditorExtensionsEditView, { model: new Backbone.Model({ _id: Origin.location.route1 }) });
   });
--- app/modules/editor/themeEditor/index.js
+++ app/modules/editor/themeEditor/index.js
@@ -2,14 +2,12 @@
 define(function(require) {
   var Origin = require('core/origin');
   var EditorThemingView = require('./views/editorThemingView.js');
-  var EditorThemingSidebarView = require('./views/editorThemingSidebarView.js');
 
   Origin.on('editorCommon:theme', function() {
     Origin.router.navigate(`#/editor/${Origin.editor.data.course.get('_id')}/${ROUTE}`, { trigger: true });
   });
 
   Origin.on('editor:selecttheme', () => {
-    Origin.sidebar.addView(new EditorThemingSidebarView().$el);
     Origin.contentPane.setView(EditorThemingView);
   });
 });
