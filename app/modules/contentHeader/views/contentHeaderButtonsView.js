a392453f9c58eb4fadd1cb2f99696321dec24dc1
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-07 01:56:46 +0000

    Fix event name

--- app/modules/contentHeader/views/contentHeaderButtonsView.js
+++ app/modules/contentHeader/views/contentHeaderButtonsView.js
@@ -42,7 +42,7 @@ define(function(require) {
       $item = $(event.currentTarget).parent('.item');
       const data = this.data.groups[$item.attr('data-group')].items[$item.attr('data-item')];
       let eventName = this.data.type;
-      if(data.eventName) eventName += data.eventName;
+      if(data.eventName) eventName += `:${data.eventName}`;
       Origin.trigger(eventName, data.eventData);
     }
   });
