0037a223e720866600be77aa98266ac21dcbf7aa
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-09 10:35:42 +0000

    Remove unused code

--- app/modules/editor/themeEditor/index.js
+++ app/modules/editor/themeEditor/index.js
@@ -3,11 +3,5 @@ define(function(require) {
   var Origin = require('core/origin');
   var EditorThemingView = require('./views/editorThemingView.js');
 
-  Origin.on('editorCommon:theme', function() {
-    Origin.router.navigate(`#/editor/${Origin.editor.data.course.get('_id')}/${ROUTE}`, { trigger: true });
-  });
-
-  Origin.on('editor:selecttheme', () => {
-    Origin.contentPane.setView(EditorThemingView);
-  });
+  Origin.on('editor:selecttheme', () => Origin.contentPane.setView(EditorThemingView));
 });
