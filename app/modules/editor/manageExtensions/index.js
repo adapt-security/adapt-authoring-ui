292cf36b0e3ccf4a12db7fe82871f5030ae15a79
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-09 10:34:43 +0000

    Separate contentObject files into their individual pages

--- app/modules/editor/manageExtensions/index.js
+++ app/modules/editor/manageExtensions/index.js
@@ -1,6 +1,5 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Backbone = require('backbone');
   var EditorExtensionsEditView = require('./views/editorExtensionsEditView');
   var Origin = require('core/origin');
 
@@ -9,6 +8,6 @@ define(function(require) {
       breadcrumbs: ['dashboard', 'course', { title: Origin.l10n.t('app.editorextensions') }],
       title: Origin.editor.data.course.get('title')
     });
-    Origin.contentPane.setView(EditorExtensionsEditView, { model: new Backbone.Model({ _id: Origin.location.route1 }) });
+    Origin.contentPane.setView(EditorExtensionsEditView, { model: Origin.editor.data.course });
   });
 });
