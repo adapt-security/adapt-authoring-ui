217b575bfa6624bf113778b26b312eb46fb65b5a
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-10 16:39:59 +0000

    Refactor filtersView

--- app/modules/contentHeader/views/filtersView.js
+++ app/modules/contentHeader/views/filtersView.js
@@ -5,26 +5,41 @@ define(function(require) {
   
   var FiltersView = ContentHeaderToggleView.extend({
     async preRender() {
-      let hasTags = false;
-      this.data.groups.forEach(({ items }) => {
-        items.forEach(i => {
-          if(i.type === 'tags') hasTags = true;
-        });
+      if(this.data.groups.some(({ items }) => items.some(i => i.type === 'tags'))) {
+        this.data.tags = (await $.post('api/tags/query'));
+      }
+    },
+    getFilterData() {
+      const data = {};
+      this.data.groups.forEach((group) => {
+        if(group.id) data[group.id] = {};
+        const dataObj = group.id ? data[group.id] : data;
+        group.items.forEach(item => dataObj[item.id] = this.getItemValue(group, item));
       });
-      if(hasTags) this.data.tags = (await $.post('api/tags/query'));
+      return data;
+    },
+    getItemValue(group, item) {
+      const $item = $(`.group[data-index="${group.index}"] .item[data-index="${item.index}"]`);
+      switch(item.type) {
+        case 'search':
+          return $('input', $item).val();
+        case 'toggle':
+          return !!$('input', $item).attr('checked');
+        case 'search':
+          return $('.tag.selected', $item).toArray().map(t => $(t).attr('data-value'));
+      }
     },
     onItemClicked(event) {
-      event.stopPropagation();
-      const $target = $(event.target);
-      let value = $target.val() || $target.attr('data-value');
-      
-      if($target.hasClass('tag')) {
-        $target.toggleClass('selected');
-        value = $('.tag.selected', this.$el).toArray().map(t => $(t).attr('data-value'));
+      const $target = $(event.currentTarget);
+      const type = $target.attr('data-type');
+
+      if(type === 'toggle' && $(event.target).prop('tagName') === 'input') {
+        return;
       }
-      const allFilterData = {};
-      const eventName = `${this.data.type}:${$(event.currentTarget).attr('data-event')}`;
-      Origin.trigger(eventName, value, allFilterData);
+      if(type === 'tag') {
+        $target.toggleClass('selected');
+      }
+      Origin.trigger(this.data.type, this.getFilterData());
     }
   }, {
     defaultButtonIcon: 'fa-filter',
