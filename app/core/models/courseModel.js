3e0a9138d2ccd21254960860e4c33ac86866e1f9
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-13 13:22:35 +0000

    Remove unused functions

--- app/core/models/courseModel.js
+++ app/core/models/courseModel.js
@@ -1,23 +1,10 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
-  var Origin = require('core/origin');
-  var Helpers = require('core/helpers');
   var ContentModel = require('./contentModel');
 
   var CourseModel = ContentModel.extend({
     _type: 'course',
-    _childTypes: 'contentobject',
-
-    getHeroImageURI: function () {
-      if(Helpers.isAssetExternal(this.get('heroImage'))) {
-        return this.get('heroImage');
-      }
-      return `api/assets/serve/${this.get('heroImage')}?thumb=true`;
-    },
-
-    isEditable: function () {
-      return this.get('_isShared') || this.get('createdBy') == Origin.sessionModel.get('id');
-    }
+    _childTypes: 'contentobject'
   });
 
   return CourseModel;
