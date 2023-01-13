c3de32fadf2344dfe79e2c891cc7099d4da582f2
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-13 13:25:22 +0000

    Add content type data

--- app/core/models/contentModel.js
+++ app/core/models/contentModel.js
@@ -18,3 +18,64 @@ define(function(require) {
 
   return ContentModel;
 });
+
+const ContentAttributes = {
+  article: {
+    _type: 'article',
+    _parentType: 'contentobject',
+    _siblingTypes: 'article',
+    _childTypes: 'block',
+    defaults: {
+      _isCollapsible: true,
+      _isCollapsed: false
+    }
+  },
+  block: {
+    _type: 'block',
+    _parentType: 'article',
+    _siblingTypes: 'block',
+    _childTypes: 'component',
+    // Block specific properties
+    layoutOptions:  null,
+    dragLayoutOptions: null,
+    attributeBlacklist: [
+      '_isDeleted',
+      '_tenantId',
+      '_trackingId',
+      'createdBy',
+      'createdAt',
+      'layoutOptions',
+      'updatedAt'
+    ]
+  },
+  component: {
+    _type: 'component',
+    _parentType: 'block',
+    _siblingTypes: 'component',
+    attributeBlacklist: [
+      '_isDeleted',
+      'createdBy',
+      'createdAt',
+      'updatedAt'
+    ]
+  },
+  config: {
+    _type: 'config',
+    _parent: 'course'
+  },
+  contentObject: {
+    _type: 'contentobject',
+    _parentType: 'contentobject',
+    _siblingTypes: 'contentobject',
+    _childTypes: ['contentobject', 'article'],
+
+    defaults: {
+      _isSelected: false,
+      _isExpanded: false
+    }
+  },
+  course: {
+    _type: 'course',
+    _childTypes: 'contentobject'
+  }
+};
