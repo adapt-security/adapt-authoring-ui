715dcc0176f30d5747ea395a74750f808de5aacb
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-13 13:24:55 +0000

    Add ApiModel

---
+++ app/core/models/apiModel.js
@@ -0,0 +1,23 @@
+// LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
+define(function(require) {
+  var Backbone = require('backbone');
+
+  var ContentModel = Backbone.Model.extend({
+    idAttribute: '_id',
+    attributeBlacklist: null,
+    
+    constructor: function(attributes, options) {
+      super(attributes, options);
+      this.urlRoot = `api/${options.type}`;
+    },
+
+    serialize: function() {
+      return JSON.stringify(this);
+    },
+    pruneAttributes: function() {
+      if(this.attributelacklist) this.attributeBlacklist.forEach(this.unset);
+    }
+  });
+
+  return ContentModel;
+});
