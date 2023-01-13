3ad59f445990da4634fcb5cd907706b6264529a6
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-13 13:09:57 +0000

    Update import path

--- app/core/collections/contentCollection.js
+++ app/core/collections/contentCollection.js
@@ -1,6 +1,6 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define([
-  '../collections/apiCollection',
+  './apiCollection',
   '../models/contentModel',
   '../origin',
   '../helpers',
