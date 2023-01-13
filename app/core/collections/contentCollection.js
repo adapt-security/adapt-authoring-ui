5cd6d8da0da24e42b871e9e230305bc43e37a33f
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-13 15:04:20 +0000

    Remove unnecessary code

--- app/core/collections/contentCollection.js
+++ app/core/collections/contentCollection.js
@@ -14,12 +14,12 @@ define([
       ApiCollection.prototype.initialize.apply(this, arguments);
     
       this._type = options._type;
-      if(this._type) this.model = ContentModel({ _type: this._type });
       this._courseId = options._courseId;
       this._parentId = options._parentId;
     
       this.on('reset', () => Origin.trigger('contentCollection:dataLoaded', this._type));
     },
+
     buildQuery: function() {
       var query = ApiCollection.prototype.buildQuery.apply(this, arguments);
       if(this._type) query._type = this._type;
