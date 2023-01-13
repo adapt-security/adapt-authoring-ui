21c05f28eeae244e88c37039ff444ec8539ee3af
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-13 12:28:27 +0000

    Update index in-line with refactoring

--- app/modules/userManagement/index.js
+++ app/modules/userManagement/index.js
@@ -1,12 +1,8 @@
 define(function(require) {
+  var Backbone = require('backbone');
   var Origin = require('core/origin');
   var UserManagementView = require('./views/userManagementView');
   var AddUserView = require('./views/addUserView');
-  var UserCollection = require('./collections/userCollection');
-
-  var isReady = false;
-  var allRoles = new Backbone.Collection();
-  var userCollection = new UserCollection();
 
   var scopes = ["write:users"];
 
@@ -23,68 +19,18 @@ define(function(require) {
     });
   });
 
-  Origin.on('origin:dataReady login:changed', function() {
-    allRoles.on('sync', function() {
-      isReady = true;
-      Origin.trigger('userManagement:dataReady');
-    });
-    allRoles.url = 'api/roles';
-    allRoles.fetch();
-  });
-  
-
   Origin.on('router:userManagement', function(location, subLocation, action) {
-    if(isReady) return onRoute(location, subLocation, action);
-    Origin.once('userManagement:dataReady', () => onRoute(location, subLocation, action));
+    location === 'addUser' ? renderAddUser() : renderUserManagement();
   });
 
-  var onRoute = async function(location, subLocation, action) {
-    var model = new Backbone.Model({ allRoles });
+  function renderAddUser() {
+    Origin.trigger('contentHeader:updateTitle', { breadcrumbs: [{ title: Origin.l10n.t('app.usermanagement'), url: '#' }], title: Origin.l10n.t('app.addnewuser') });
+    Origin.contentHeader.setButtons(Origin.contentHeader.BUTTON_TYPES.ACTIONS, Origin.contentHeader.ACTION_BUTTON_TEMPLATES.EDIT_FORM);
+    Origin.contentPane.setView(AddUserView, { model: new Backbone.Model() });
+  }
 
-    if (location && location === 'addUser') {
-      Origin.trigger('contentHeader:updateTitle', { breadcrumbs: [{ title: Origin.l10n.t('app.usermanagement'), url: '#' }], title: Origin.l10n.t('app.addnewuser') });
-      Origin.contentHeader.setButtons(Origin.contentHeader.BUTTON_TYPES.ACTIONS, Origin.contentHeader.ACTION_BUTTON_TEMPLATES.EDIT_FORM);
-      Origin.contentPane.setView(AddUserView, { model: model });
-      return;
-    }
-    await refreshUsers();
-
+  function renderUserManagement() {
     Origin.contentHeader.setButtons(Origin.contentHeader.BUTTON_TYPES.FILTERS, [
-      {
-        id: 'role',
-        items: [
-          {
-            id: 'contentcreator',
-            type: 'toggle',
-            buttonText: 'Content creators',
-            checked: true
-          },
-          {
-            id: 'authuser',
-            type: 'toggle',
-            buttonText: 'Authenticated users',
-            checked: true
-          },
-          {
-            id: 'superuser',
-            type: 'toggle',
-            buttonText: 'Super users',
-            checked: true
-          },
-          {
-            id: 'contenteditor',
-            type: 'toggle',
-            buttonText: 'Content Editors',
-            checked: true
-          },
-          {
-            id: 'contentreviewer',
-            type: 'toggle',
-            buttonText: 'Content Reviewers',
-            checked: true
-          }
-        ]
-      },
       {
         name: Origin.l10n.t('app.search'),
         items: [
@@ -92,6 +38,50 @@ define(function(require) {
             id: 'search',
             type: 'search',
             placeholder: Origin.l10n.t('app.searchbyemail')
+          }
+        ]
+      },
+      {
+        items: [
+          {
+            id: 'isLocked',
+            buttonText: Origin.l10n.t('app.accountlocked'),
+            type: 'toggle'
+          }
+        ]
+      },
+      {
+        id: 'role',
+        items: [
+          {
+            id: 'contentcreator',
+            type: 'toggle',
+            buttonText: Origin.l10n.t('app.contentcreators'),
+            checked: true
+          },
+          {
+            id: 'authuser',
+            type: 'toggle',
+            buttonText: Origin.l10n.t('app.authusers'),
+            checked: true
+          },
+          {
+            id: 'superuser',
+            type: 'toggle',
+            buttonText: Origin.l10n.t('app.superusers'),
+            checked: true
+          },
+          {
+            id: 'contenteditor',
+            type: 'toggle',
+            buttonText: Origin.l10n.t('app.contenteditors'),
+            checked: true
+          },
+          {
+            id: 'contentreviewer',
+            type: 'toggle',
+            buttonText: Origin.l10n.t('app.contentreviews'),
+            checked: true
           }
         ]
       }
@@ -103,20 +93,7 @@ define(function(require) {
       }]
     }]);
     Origin.trigger('contentHeader:updateTitle', { breadcrumbs: [{ title: Origin.l10n.t('app.usermanagement'), url: '#' }], title: Origin.l10n.t('app.usermanagementtitle') });
-
-    Origin.contentPane.setView(UserManagementView, { model, collection: userCollection }, { fullWidth: true });
-
+    Origin.contentPane.setView(UserManagementView, {}, { fullWidth: true });
     Origin.on('actions:adduser', () => Origin.router.navigateTo('userManagement/addUser'));
-    
-    Origin.on('userManagement:refresh', refreshUsers);
-  };
-  
-  async function refreshUsers() {
-    try {
-      await userCollection.fetch();
-      userCollection.forEach(user => user.set({ allRoles }));
-    } catch(e) {
-      Origin.Notify.alert({ type: 'error', message: e });
-    }
   }
 });
