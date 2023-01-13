a7f5f6dd5e8636b9b8303beceb4ae6537c2f6ad4
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-13 13:35:17 +0000

    Add shorthand factory function

--- app/core/models/apiModel.js
+++ app/core/models/apiModel.js
@@ -18,6 +18,17 @@ define(function(require) {
       if(this.attributelacklist) this.attributeBlacklist.forEach(this.unset);
     }
   });
+  /**
+   * Shorthand for creating new ApiModels
+   */
+  const createModel = (type, data) => {
+    return new ApiModel(data, { endpoint: type });
+  };
+  ApiModel.Asset = data => createModel('assets', data);
+  ApiModel.ContentPlugin = data => createModel('contentplugins', data);
+  ApiModel.CourseThemePreset = data => createModel('coursethemepresets', data);
+  ApiModel.Tag = data => createModel('tags', data);
+  ApiModel.User = data => createModel('users', data);
 
   return ApiModel;
 });
