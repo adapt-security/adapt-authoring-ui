fde4e6eb87defca30a9c3c92a35871354075ecf4
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-16 15:00:25 +0000

    Refactor PluginTypeView

--- app/modules/pluginManagement/views/pluginTypeView.js
+++ app/modules/pluginManagement/views/pluginTypeView.js
@@ -16,10 +16,7 @@ define(function(require){
 
     preRender: function () {
       this.listenTo(this, 'remove', this.remove);
-      this.listenTo(this.model, {
-        sync: this.render,
-        destroy: this.remove
-      });
+      this.listenTo(this.model, 'sync', this.render);
     },
 
     render: function () {
@@ -29,85 +26,44 @@ define(function(require){
     },
 
     toggleEnabled: function () {
-      this.model.save({ 
-        isEnabled: this.$('.pluginType-enabled').is(':checked') 
-      }, { patch: true });
+      this.model.save({ isEnabled: this.$('.pluginType-enabled').is(':checked') }, { patch: true });
     },
 
     toggleAddedDefault: function() {
-      this.model.save({
-        isAddedByDefault: this.$('.pluginType-addedDefault').is(':checked')
-      }, { patch: true });
+      this.model.save({ isAddedByDefault: this.$('.pluginType-addedDefault').is(':checked') }, { patch: true });
     },
 
-    updatePlugin: function (event) {
+    updatePlugin: async function (event) {
       event && event.preventDefault();
       var $btn = this.$('.plugin-update');
-      var $icon = $btn.find('i');
 
       if($btn.is(':disabled')) return false;
 
-      $btn.attr({
-        disabled: true,
-        title: Origin.l10n.t('app.updating')
-      });
-      $icon.removeClass().addClass('fa fa-refresh fa-spin');
-
-      $.post(`${this.model.urlRoot}/${this.model.get('_id')}/update`)
-        .done(function() {
-          $btn.attr('title', Origin.l10n.t('app.uptodate'));
-          $icon.removeClass().addClass('fa fa-check');
-          this.model.fetch();
-        }.bind(this))
-        .fail(function(e) {
-          $btn.attr('title', Origin.l10n.t('app.updatefailed'));
-          $icon.removeClass().addClass('fa fa-times');
-          Origin.Notify.alert({ type: 'error', text: e.responseJSON.message });
-        });
-
+      $btn.attr({ disabled: true, title: Origin.l10n.t('app.updating') });
+      try {
+        await $.post(`${this.model.url()}/update`);
+      } catch(e) {
+        Origin.Notify.alert({ type: 'error', text: e.responseJSON.message });
+      }
+      this.model.fetch();
       return false;
     },
 
     deletePluginPrompt: function(event) {
       event && event.preventDefault();
-
-      $.ajax({
-        'method': 'GET',
-        'url':  this.model.urlRoot + '/' + this.model.get('_id') + '/uses'
-      }).done(function (data) {
-        const popup = {};
-
-        if (data.length === 0) {
-          Origin.Notify.confirm({
-            type: 'warning',
-            title: Origin.l10n.t('app.deleteplugin'),
-            text: Origin.l10n.t('app.confirmdeleteplugin', { plugin: this.model.get('displayName') }),
-            destructive: false,
-            callback: this.deletePluginConfirm.bind(this)
+      Origin.Notify.confirm({
+        type: 'warning',
+        title: Origin.l10n.t('app.deleteplugin'),
+        text: Origin.l10n.t('app.confirmdeleteplugin', { plugin: this.model.get('displayName') }),
+        destructive: false,
+        callback: result => {
+          if(!result.isConfirmed) return;
+          this.model.destroy({ 
+            success: () => this.remove(),
+            error: (model, jqXhr) => Origin.Notify.alert({ type: 'error', text: jqXhr.responseJSON.message })
           });
-          return;
         }
-
-        var courses = '';
-        for (var i = 0, len = data.length; i < len; i++) {
-          courses += data[i].title + ' ' + Origin.l10n.t('app.by') + ' ' + data[i].createdBy.email + '<br />'
-        }
-        popup.type = 'error';
-        popup.title = Origin.l10n.t('app.cannotdelete') + ' ' + this.model.get('displayName');
-        popup.text = Origin.l10n.t('app.coursesused') + '<br />' + courses + '<br />';
-
-        Origin.Notify.alert(popup);
-
-      }.bind(this));
-    },
-
-    deletePluginConfirm: function(result) {
-      if (!result.isConfirmed) return;
-
-      $.ajax({
-        method: 'DELETE',
-        url: this.model.urlRoot + '/' + this.model.get('_id')
-      }).done(this.remove.bind(this));
+      });
     }
   }, {
     template: 'pluginType'
