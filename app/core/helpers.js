155d73cea0d94b20adb1399049694c1e6d79afad
Thomas Taylor <hello@tomtaylor.name>
2023-01-26 15:23:24 +0000

    Refactor helper function

--- app/core/helpers.js
+++ app/core/helpers.js
@@ -312,17 +312,20 @@ define(['handlebars', 'moment', 'core/origin'], function(Handlebars, Moment, Ori
 
     submitForm($form, options = {}) {
       return new Promise(async (resolve, reject) => {
+        if($form.$el) $form = $form.$el; // for Scaffold forms
         const body = new FormData($form[0]);
-        if(options.extendedData) Object.entries(options.extendedData).forEach(([attr, val]) => body.append(attr, val));
-        const res = await fetch($form.attr('action'), { method: $form.attr('method'), body });
-        if(res.status === 204) {
-          return resolve();
+        if(options.data) {
+          Object.entries(options.data).forEach(d => body.append(...d));
+          delete options.data;
         }
-        const data = await res.json();
-        if(res.status > 299) {
-          return reject(data);
+        if(typeof options.beforeSubmit === 'function') {
+          options.beforeSubmit(body);
+          delete options.beforeSubmit;
         }
-        resolve(data);
+        const reqOptions = Object.assign({ method: $form.attr('method'), body }, options);
+        const res = await fetch(options.url || $form.attr('url'), reqOptions);
+        const data = res.body ? await res.json() : undefined;
+        res.status > 299 ? reject(data) : resolve(data);
       });
     },
     ajaxSubmit($form, options = {}) {
