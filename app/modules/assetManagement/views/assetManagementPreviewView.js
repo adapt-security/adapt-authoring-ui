806dff0282cc36f4ae494720e86e3476f0f4ee2a
Thomas Taylor <hello@tomtaylor.name>
2023-02-15 11:58:12 +0000

    Fix delete error

--- app/modules/assetManagement/views/assetManagementPreviewView.js
+++ app/modules/assetManagement/views/assetManagementPreviewView.js
@@ -56,7 +56,7 @@ define(function(require){
       } catch(e) {
         Origin.Notify.alert({
           type: 'error',
-          text: Origin.l10n.t('app.errordeleteasset', { message: e.responseJSON && e.responseJSON.message || e.responseText })
+          text: e.responseJSON && e.responseJSON.message || e.responseText
         });
       }
     }
