809546742d3094ccfd51870c1cd8779f09519515
Thomas Taylor <hello@tomtaylor.name>
2023-03-23 14:41:41 +0000

    Fix issue with duplicate sidebar buttons

--- app/modules/frameworkImport/index.js
+++ app/modules/frameworkImport/index.js
@@ -5,19 +5,21 @@ define(function(require) {
   var FrameworkImportSidebarView = require('./views/frameworkImportSidebarView.js');
 
   var featureScopes = ["import:adapt"];
+  var hasPermission = false;
 
   Origin.on('origin:dataReady login:changed', function init() {
     Origin.router.restrictRoute('frameworkImport', featureScopes);
-    
-    if(!Origin.sessionModel.hasScopes(featureScopes)) {
-      return;
-    }
-    Origin.on('router:frameworkImport', renderMainView);
-    Origin.on('projects:postRender', data => data.action !== 'edit' && renderImportButton(data));
+    hasPermission = Origin.sessionModel.hasScopes(featureScopes);
   });
+
+  Origin.on('router:frameworkImport', renderMainView);
+  Origin.on('projects:postRender', data => hasPermission && data.action !== 'edit' && renderImportButton(data));
 
   function renderImportButton(data) {
     $('button.projects-sidebar-import-course').remove(); // remove existing button
+    if(!hasPermission) {
+      return;
+    }
     var $btn = $(Handlebars.partials.part_frameworkImportButton());
     $('.projects-sidebar-add-course').after($btn);
     $btn.click(() => Origin.router.navigateTo('frameworkImport'));
