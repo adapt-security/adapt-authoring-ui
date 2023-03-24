e43eae697cfca6a96f3667ab4533669d71b0eb34
Chris Steele <chris.steele@kineo.com>
2023-03-24 11:11:09 +0000

    Amend syntax to avoid rollup warning (https://rollupjs.org/troubleshooting/#error-this-is-undefined)

--- app/core/helpers.js
+++ app/core/helpers.js
@@ -304,7 +304,7 @@ define(['handlebars', 'moment', 'core/origin'], function(Handlebars, Moment, Ori
       return '';
     },
     // Comparison operator (ifValueEquals left for compatibility)
-    when: (a, operator, b, block) => {
+    when: function(a, operator, b, block) {
       console.log(a, operator, b);
       const ops = {
         eq: (l,r) => l === r,
