b5307c85dfcc894e49d5a09443671e1fec6aa975
Chris Steele <chris.steele@kineo.com>
2025-03-28 13:30:11 +0000

    Fix asset selection bug

--- app/modules/scaffold/views/scaffoldAssetView.js
+++ app/modules/scaffold/views/scaffoldAssetView.js
@@ -125,7 +125,7 @@ define([
           })
         }
       });
-      Origin.on('modal:actions', action => {
+      Origin.once('modal:actions', action => {
         if(action !== 'upload') Origin.modal.close();
         const selected = Origin.modal.view.collectionView.getSelected();
         if(action === 'cancel' || !selected) {
