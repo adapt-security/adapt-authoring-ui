2cdf9a5ab6060ed8f68d2fbc46cc3a73b51b0d7a
Thomas Taylor <thomas.taylor@kineo.com>
2026-03-11 01:29:51 +0000

    New: Use content tree API for editor data loading (refs #458)
    Replace full content fetch with lightweight tree endpoint. Editor now
    loads projected fields via GET /api/content/tree/:courseId and fetches
    full documents on demand when editing individual items. Staleness check
    uses If-Modified-Since conditional requests for zero-cost polling.

--- app/modules/editor/global/editorDataLoader.js
+++ app/modules/editor/global/editorDataLoader.js
@@ -1,92 +1,107 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
-define(function(require) {
-  var _ = require('underscore');
-  var ContentCollection = require('core/collections/contentCollection');
-  var ContentPluginCollection = require('core/collections/contentPluginCollection');
-  var Origin = require('core/origin');
+define(function (require) {
+  var _ = require('underscore')
+  var Backbone = require('backbone')
+  var ContentModel = require('core/models/contentModel')
+  var ContentPluginCollection = require('core/collections/contentPluginCollection')
+  var Origin = require('core/origin')
 
-  var isLoaded;
+  var isLoaded
+  var lastModified
 
   var Preloader = {
     /**
     * Loads course-specific data
     * Accepts callback for editor:refreshData
     */
-    load: async function(callback) {
-      if(!Origin.sessionModel.get('isAuthenticated')) {
-        return;
+    load: async function (callback) {
+      if (!Origin.sessionModel.get('isAuthenticated')) {
+        return
       }
-      if(!Origin.editor) Origin.editor = {};
-      if(!Origin.editor.data) Origin.editor.data = {};
+      if (!Origin.editor) Origin.editor = {}
+      if (!Origin.editor.data) Origin.editor.data = {}
 
-      isLoaded = false;
+      isLoaded = false
 
-      if(await isOutdated()) {
+      if (await isOutdated()) {
         try {
           await Promise.all([
-            new Promise (async (resolve) => {
-              const content = new ContentCollection(undefined, { _courseId: Origin.location.route1 });
-              content.queryOptions = { limit: 0 };
-              await content.fetch();
-              Origin.editor.data.content = content;
-              Origin.editor.data.course = content.findWhere({ _type: 'course' });
-              Origin.editor.data.config = content.findWhere({ _type: 'config' });
-              if(!Origin.editor.data.course || !Origin.editor.data.config) {
-                return handleError();
-              }
-              resolve()
-            }),
-            new Promise (async (resolve) => {
-              const componentTypes = new ContentPluginCollection(undefined, { type: 'component' });
-              await componentTypes.fetch();
-              Origin.editor.data.componentTypes = componentTypes;
-              resolve()
-            })
-          ]);
-        } catch(e) {
-          return handleError();
+            loadTree(),
+            loadComponentTypes()
+          ])
+        } catch (e) {
+          return handleError()
         }
       }
-      isLoaded = true;
+      isLoaded = true
 
-      Origin.editor.data.lastFetch = new Date().toISOString();
-      if(_.isFunction(callback)) {
-        callback();
+      if (_.isFunction(callback)) {
+        callback()
       }
-      Origin.trigger('editor:dataLoaded');
+      Origin.trigger('editor:dataLoaded')
     },
-    resetCourseData: function() {
-      if(Origin.editor) {
-        Origin.editor.data.course = undefined;
-        Origin.editor.data.config = undefined;
+    resetCourseData: function () {
+      if (Origin.editor) {
+        Origin.editor.data.course = undefined
+        Origin.editor.data.config = undefined
       }
+      lastModified = undefined
     },
     /**
     * Makes sure all data has been loaded and calls callback
     */
-    waitForLoad: function(callback) {
-      isLoaded ? callback.apply(this) : Origin.once('editor:dataLoaded', callback.bind(this));
+    waitForLoad: function (callback) {
+      isLoaded ? callback.apply(this) : Origin.once('editor:dataLoaded', callback.bind(this))
     }
-  };
+  }
 
-  async function isOutdated() {
+  async function loadTree () {
+    var courseId = Origin.location.route1
+    var res = await fetch('/api/content/tree/' + courseId)
+    if (!res.ok) throw new Error('Failed to fetch content tree')
+    lastModified = res.headers.get('Last-Modified')
+    var items = await res.json()
+    var content = new Backbone.Collection(items.map(function (item) {
+      return new ContentModel(item)
+    }), { comparator: '_sortOrder' })
+    content.findWhere = Backbone.Collection.prototype.findWhere
+    content.where = Backbone.Collection.prototype.where
+    Origin.editor.data.content = content
+    Origin.editor.data.course = content.findWhere({ _type: 'course' })
+    Origin.editor.data.config = content.findWhere({ _type: 'config' })
+    if (!Origin.editor.data.course || !Origin.editor.data.config) {
+      return handleError()
+    }
+  }
+
+  async function loadComponentTypes () {
+    var componentTypes = new ContentPluginCollection(undefined, { type: 'component' })
+    await componentTypes.fetch()
+    Origin.editor.data.componentTypes = componentTypes
+  }
+
+  async function isOutdated () {
     try {
-      if(Origin.editor.data.course.get('_id') !== Origin.location.route1) {
-        Origin.editor.data.lastFetch = 0
-        return true;
+      if (Origin.editor.data.course.get('_id') !== Origin.location.route1) {
+        lastModified = undefined
+        return true
       }
-    } catch(e) {
-      Origin.editor.data.lastFetch = 0
-      return true;
+    } catch (e) {
+      lastModified = undefined
+      return true
     }
-    const [latestDoc] = await $.post('/api/content/query?sort={%22updatedAt%22:-1}&limit=1', {_courseId:Origin.editor.data.course.get('_id')});
-    return !latestDoc || new Date(Origin.editor.data.lastFetch) < new Date(latestDoc.updatedAt);
+    if (!lastModified) return true
+    var courseId = Origin.editor.data.course.get('_id')
+    var res = await fetch('/api/content/tree/' + courseId, {
+      headers: { 'If-Modified-Since': lastModified }
+    })
+    return res.status !== 304
   }
 
-  function handleError() {
-    Origin.Notify.alert({ type: 'error', text: 'Failed to fetch course data' });
-    Origin.router.navigateTo('projects');
+  function handleError () {
+    Origin.Notify.alert({ type: 'error', text: 'Failed to fetch course data' })
+    Origin.router.navigateTo('projects')
   }
 
-  return Preloader;
-});
+  return Preloader
+})
